36e0cf091de523952362f51b3e6e943fc4a3c998
aboud20-meet <aboud20@meet.mit.edu>
2018-08-09 10:29:07 +0300

    labs

--- exercises/app.py
+++ exercises/app.py
@@ -1,5 +1,5 @@
 from databases import *
-from flask import Flask, render_template, url_for, request 
+from flask import Flask, render_template, url_for, request, redirect 
 app = Flask(__name__)
 
 @app.route('/')
@@ -17,11 +17,15 @@ def add_student_route():
 	return render_template('add.html')
 
 
-	
+@app.route('/delete/<int:student_id>',methods = ['POST'])	
+def delete(student_id):
+	delete_student(student_id)
+	return redirect(url_for("home"))
 
 
 @app.route('/student/<int:student_id>')
 def display_student(student_id):
-    return render_template('student.html', student=query_by_id(student_id))
+    return render_template('student.html', student=query_by_id(student_id), id= student_id
+    	)
 
 app.run(debug=True)
